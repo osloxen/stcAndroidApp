0e2a9c6889f24ce960bb2b76bc258d11545a51c1
David Berge <david.berge@intel.com>
2018-01-09 08:01:04 -0800

    initial comit

---
+++ app/src/main/java/com/locallinkonline/stcatherineschool/HomeActivity.java
@@ -0,0 +1,89 @@
+package com.locallinkonline.stcatherineschool;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class HomeActivity extends AppCompatActivity implements
+                                                        GradesHomeworkFragment.OnFragmentInteractionListener,
+                                                        ActivitiesFragment.OnFragmentInteractionListener,
+                                                        LunchFragment.OnFragmentInteractionListener,
+                                                        HomeworkForGradeFragment.OnFragmentInteractionListener {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+        setupNavigationView();
+    }
+
+
+
+    private void setupNavigationView() {
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.navigation);
+        if (bottomNavigationView != null) {
+            // Select first menu item by default and show Fragment accordingly.
+            Menu menu = bottomNavigationView.getMenu();
+            selectFragment(menu.getItem(0));
+            // Set action to perform when any menu-item is selected.
+            bottomNavigationView.setOnNavigationItemSelectedListener(
+                    new BottomNavigationView.OnNavigationItemSelectedListener() {
+                        @Override
+                        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                            selectFragment(item);
+                            return false;
+                        }
+                    });
+        }
+    }
+
+
+
+    protected void selectFragment(MenuItem item) {
+        item.setChecked(true);
+        switch (item.getItemId()) {
+            case R.id.navigation_homework:
+                pushFragment(new GradesHomeworkFragment());
+                break;
+            case R.id.navigation_activities:
+                pushFragment(new ActivitiesFragment());
+                break;
+            case R.id.navigation_lunch:
+                pushFragment(new LunchFragment());
+                break;
+        }
+    }
+
+
+
+    protected void pushFragment(Fragment fragment) {
+        if (fragment == null)
+            return;
+
+        FragmentManager fragmentManager = getFragmentManager();
+        if (fragmentManager != null) {
+            FragmentTransaction ft = fragmentManager.beginTransaction();
+            if (ft != null) {
+                ft.replace(R.id.rootActivityView, fragment);
+                ft.commit();
+            }
+        }
+    }
+
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+}
---
+++ app/src/main/java/com/locallinkonline/stcatherineschool/HomeworkForGradeFragment.java
@@ -0,0 +1,272 @@
+package com.locallinkonline.stcatherineschool;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.JsonReader;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.net.ssl.HttpsURLConnection;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link HomeworkForGradeFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link HomeworkForGradeFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class HomeworkForGradeFragment extends android.app.Fragment {
+
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_GRADE = "param1";
+
+    // TODO: Rename and change types of parameters
+    public  String grade;
+
+    public void setGrade(String gradeFromListView) {
+        grade = gradeFromListView;
+    }
+
+
+    String[] mItems = {"Loading..."};
+    String[] columnNames = getColumnValues();
+    //String[] columnNames;
+    ListView listView;
+    ArrayAdapter<String> listViewAdapter;
+
+
+    public String[] getColumnValues() {
+
+        final List<String> columnNames = new ArrayList<String>();
+
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                // All your networking logic
+                // should be here
+
+                // Create URL
+                URL githubEndpoint = null;
+                try {
+                    //githubEndpoint = new URL("https://api.github.com/");
+                    githubEndpoint = new URL("https://telbelahfa.execute-api.us-east-1.amazonaws.com/prod/stc/columns/grade6");
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+
+                // Create connection
+                HttpsURLConnection myConnection;
+                try {
+                    myConnection = (HttpsURLConnection) githubEndpoint.openConnection();
+
+                    if (myConnection.getResponseCode() == 200) {
+                        // Success
+                        InputStream responseBody = myConnection.getInputStream();
+                        InputStreamReader responseBodyReader =
+                                new InputStreamReader(responseBody, "UTF-8");
+
+                        JsonReader jsonReader = new JsonReader(responseBodyReader);
+
+                        jsonReader.beginObject(); // Start processing the JSON object
+                        while (jsonReader.hasNext()) { // Loop through all keys
+
+                            String key = jsonReader.nextName(); // Fetch the next key
+
+
+                            List<String> columns = new ArrayList<String>();
+
+                            if (key.equals("grade")) {
+                                jsonReader.skipValue();
+                            } else if (key.equals("numColumns")) {
+                                jsonReader.skipValue();
+                            } else if (key.equals("columnArray")) {
+                                jsonReader.beginArray();
+                                while (jsonReader.hasNext()) {
+                                    columnNames.add(jsonReader.nextString());
+                                }
+                                jsonReader.endArray();
+                            } else {
+                                jsonReader.skipValue();
+                            }
+
+                            Log.d("getColumnVals:", Arrays.toString(columnNames.toArray()));
+                        }
+
+                        mItems = columnNames.toArray(new String[(columnNames.size())]);
+
+
+                    } else {
+                        // Error handling code goes here
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        });
+
+        return columnNames.toArray(new String[columnNames.size()]);
+    }
+
+
+
+
+
+
+    private OnFragmentInteractionListener mListener;
+
+    public HomeworkForGradeFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param grade Parameter 1.
+     * @return A new instance of fragment HomeworkForGradeFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static HomeworkForGradeFragment newInstance(String grade) {
+        HomeworkForGradeFragment fragment = new HomeworkForGradeFragment();
+        Bundle args = new Bundle();
+
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        Log.d("grade",grade);
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_homework_for_grade, container, false);
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+
+
+
+
+
+    private class GetColumnData extends AsyncTask<Void,Void,Void> {
+
+
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Log.d("preExecute", Arrays.toString(mItems));
+
+        }
+
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            final List<String> columnNames = new ArrayList<String>();
+
+
+            AsyncTask.execute(new Runnable() {
+                @Override
+                public void run() {
+
+                    HomeworkForGradeFragment.this.getColumnValues();
+
+                }
+
+            });
+
+            return null;
+        }
+
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+
+            listViewAdapter = new ArrayAdapter<String>(
+                    getActivity(),
+                    android.R.layout.simple_list_item_1,
+                    mItems
+            );
+
+            Log.d("postExecute", Arrays.toString(mItems));
+
+            HomeworkForGradeFragment.this.listView.setAdapter(listViewAdapter);
+
+
+        }
+
+    }
+
+
+
+}
