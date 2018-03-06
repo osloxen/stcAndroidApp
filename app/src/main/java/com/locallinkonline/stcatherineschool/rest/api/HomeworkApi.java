4714f0285f80254752e32f1f4fd83a1368b014a9
Erick Spaan <espaan@carmens-mbp-2.lan>
2018-03-05 22:20:39 -0800

    Adding example grade level homework apis

--- app/src/main/java/com/locallinkonline/stcatherineschool/rest/api/HomeworkApi.java
+++ app/src/main/java/com/locallinkonline/stcatherineschool/rest/api/HomeworkApi.java
@@ -1,8 +1,25 @@
 package com.locallinkonline.stcatherineschool.rest.api;
 
+import com.locallinkonline.stcatherineschool.rest.model.EighthGradeHomework;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
 /**
  * Created by espaan on 3/5/18.
  */
 
 public interface HomeworkApi {
+
+    @GET("homework/grade7")
+    Call<List<EighthGradeHomework>> getSeventhGradeHomework(@Query("startDate") String startDate,
+                                                            @Query("endDate") String endDate);
+
+    @GET("homework/grade8")
+    Call<List<EighthGradeHomework>> getEighthGradeHomework(@Query("startDate") String startDate,
+                                                           @Query("endDate") String endDate);
 }
--- app/src/main/java/com/locallinkonline/stcatherineschool/rest/model/Sport.java
+++ app/src/main/java/com/locallinkonline/stcatherineschool/rest/model/Sport.java
@@ -4,5 +4,5 @@ package com.locallinkonline.stcatherineschool.rest.model;
  * Created by espaan on 3/5/18.
  */
 
-public class Homework {
+public class Sport {
 }
